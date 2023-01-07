464928f05d009fc6c45b164b2a0dcdf7244e10f1
PratikshaSurwade <89350917+PratikshaSurwade@users.noreply.github.com>
2023-01-07 11:01:09 +0530

    Deployment ready

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:3000"
-    : "https://mini-social-media-mern.herokuapp.com/";
+    : "https://mini-social-media.onrender.com/";
 
 module.exports = baseUrl;
